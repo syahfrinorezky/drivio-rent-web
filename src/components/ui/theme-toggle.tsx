5aba3390535202072cee04dc1b883b2902b83b18
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-15 04:34:35 +0800

    feat: add theme toggle

---
+++ src/components/ui/theme-toggle.tsx
@@ -0,0 +1,34 @@
+/** @format */
+
+"use client";
+
+import React from "react";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { HiMoon, HiSun } from "react-icons/hi2";
+
+function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className="relative flex items-center justify-center w-10 h-10 rounded-full cursor-pointer"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <HiMoon className="absolute text-xl transition-all duration-300 ease-in-out rotate-90 scale-0 opacity-0 dark:rotate-0 dark:scale-100 dark:opacity-100" />
+      <HiSun className="absolute text-xl transition-all duration-300 ease-in-out rotate-0 scale-100 opacity-100 dark:rotate-90 dark:scale-0 dark:opacity-0" />
+    </button>
+  );
+}
+
+export default ThemeToggle;
