e98fc22376276f4d35d1502fee6cfb86bb41f115
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-17 19:09:32 +0800

    feat: add desktop view/layout of header for desktop

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,50 +1,82 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 import { motion, AnimatePresence } from "framer-motion";
-import ThemeToggle from "@/components/ui/theme-toggle";
 import { HiBars3BottomRight, HiXMark } from "react-icons/hi2";
 import { NAVIGATION_PUBLIC } from "@/constants/navigation";
 import { usePathname } from "next/navigation";
 
+import useIsScrolled from "@/hooks/isScrolled";
+import { Button } from "@/components/ui/button";
+
 function Header() {
   const pathname = usePathname();
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const isScrolled = useIsScrolled();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isMenuOpen]);
 
   return (
     <>
       <div
-        className={clsx(
-          "fixed top-0 w-full bg-white dark:bg-neutral-900 shadow-md z-40 "
-        )}
+        className={clsx("fixed top-0 w-full bg-white shadow-lg z-40", {
+          "bg-white/40 backdrop-blur": isScrolled,
+        })}
       >
-        <div className="container mx-auto px-4 py-3 md:p-3 flex justify-between items-center">
+        <div className="px-4 py-3 md:container md:mx-auto md:max-w-[1200px] flex justify-between items-center">
           <Link href="/" className="flex items-center gap-x-2">
             <Image
               src="/logo/dark.svg"
               alt="Drivio"
               width={100}
               height={100}
-              className="w-15 block dark:hidden"
+              className="w-15 block"
             />
-            <Image
-              src="/logo/white.svg"
-              alt="Drivio"
-              width={100}
-              height={100}
-              className="w-15 hidden dark:block"
-            />
-            <h1 className="text-black dark:text-white font-bold text-2xl font-poppins hidden lg:block">
+            <h1 className="text-black font-bold text-2xl font-poppins hidden lg:block">
               Drivio
             </h1>
           </Link>
 
           <button className="sm:hidden" onClick={() => setIsMenuOpen(true)}>
-            <HiBars3BottomRight className="text-white w-8 h-8" />
+            <HiBars3BottomRight className="text-black w-8 h-8" />
           </button>
+
+          <div className="hidden md:flex gap-x-3">
+            {NAVIGATION_PUBLIC.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={clsx(
+                  "text-black font-medium px-4 py-2 rounded-md hover:bg-neutral-200 transition-colors duration-300 ease-in-out",
+                  pathname === item.href &&
+                    (isScrolled ? "bg-neutral-200/80" : "bg-neutral-200")
+                )}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+
+          <div className="hidden md:block">
+            <Link href={"/register"}>
+              <Button variant="default" className="cursor-pointer">
+                LOGIN
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -56,7 +88,7 @@ function Header() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
             onClick={() => setIsMenuOpen(false)}
-            className="fixed inset-0 bg-black/20 backdrop-blur-sm z-50 md:hidden"
+            className="fixed inset-0 bg-black/20 backdrop-blur-sm z-50 font-poppins md:hidden"
           >
             <motion.div
               initial={{ x: 200 }}
@@ -64,25 +96,22 @@ function Header() {
               exit={{ x: 200 }}
               transition={{ type: "spring", stiffness: 300, damping: 30 }}
               onClick={(e) => e.stopPropagation()}
-              className="fixed top-0 right-0 bg-white dark:bg-neutral-800 h-full w-3/4 p-6 z-50"
+              className="fixed top-0 right-0 bg-white h-full w-3/4 p-6 z-50"
             >
-              <div className="w-full flex flex-col">
-                <div className="flex justify-between mb-4">
-                  <ThemeToggle />
+              <div className="w-full h-full flex flex-col">
+                <div className="flex justify-end mb-4 w-full">
                   <button onClick={() => setIsMenuOpen(false)}>
-                    <HiXMark className="text-black dark:text-white w-8 h-8" />
+                    <HiXMark className="text-black w-8 h-8" />
                   </button>
                 </div>
-
                 <div className="flex flex-col mt-8">
                   {NAVIGATION_PUBLIC.map((item) => (
                     <Link
                       key={item.name}
                       href={item.href}
                       className={clsx(
-                        "text-black dark:text-white text-lg font-medium px-4 py-5 rounded-md hover:bg-neutral-200 dark:hover:bg-neutral-900 transition-colors duration-300 ease-in-out",
-                        pathname === item.href &&
-                          "bg-neutral-200 dark:bg-neutral-900"
+                        "text-black font-medium px-4 py-5 rounded-md hover:bg-neutral-200 transition-colors duration-300 ease-in-out",
+                        pathname === item.href && "bg-neutral-200"
                       )}
                       onClick={() => setIsMenuOpen(false)}
                     >
@@ -94,6 +123,22 @@ function Header() {
                       </div>
                     </Link>
                   ))}
+                </div>
+
+                <div className="mt-auto w-full flex flex-col gap-y-3 items-end">
+                  <Link href={"/register"} className="w-full">
+                    <Button variant="default" className="w-full h-12 text-base">
+                      Register
+                    </Button>
+                  </Link>
+                  <Link href={"/login"} className="w-full">
+                    <Button
+                      variant="outline"
+                      className="w-full h-12 text-base border border-accent-foreground"
+                    >
+                      Login
+                    </Button>
+                  </Link>
                 </div>
               </div>
             </motion.div>
