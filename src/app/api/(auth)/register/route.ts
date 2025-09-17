e3a45bc5683b22bed5ea6697c4a65b683bf75af0
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-17 21:00:11 +0800

    feat: add register api

---
+++ src/app/api/(auth)/register/route.ts
@@ -0,0 +1,70 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
+const registerSchema = z.object({
+  email: z.string().email("Email must be a valid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number"),
+});
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const parse = registerSchema.safeParse(body);
+
+    if (!parse.success) {
+      return NextResponse.json(
+        { message: parse.error.flatten().fieldErrors },
+        { status: 400 }
+      );
+    }
+
+    const { email, password } = parse.data;
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+
+    const existingAccount = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (existingAccount) {
+      return NextResponse.json(
+        { message: "Email already registered" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.users.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Register successful",
+        user: { id: user.id, email: user.email },
+      },
+      { status: 201 }
+    );
+  } catch {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
